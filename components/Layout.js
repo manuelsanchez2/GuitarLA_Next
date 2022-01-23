2c7a5202bc7f0774a59c16490c4375f86917715d
manuelsanchez2 <manusansan22@gmail.com>
2022-01-23 16:43:32 +0100

    Fix

--- components/Layout.js
+++ components/Layout.js
@@ -10,7 +10,7 @@ const Layout = ({ children, page, guitar }) => {
         <meta name="description" content="Sitio web de guitarras" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400;1,700&display=swap"
           rel="stylesheet"
